cd98539c74d0165af2e645c0741b1a2fa9cf6296
axel.langenskiold.2531 <ax2531la-s@student.lu.se>
2023-12-17 17:12:31 +0100

    started on factory class

--- app/src/main/java/calculator/expr/Factory.java
+++ app/src/main/java/calculator/expr/Factory.java
@@ -1,5 +1,7 @@
 package calculator.expr;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 import java.util.regex.Pattern;
 import java.io.IOException;
@@ -15,8 +17,17 @@ public class Factory {
 
 
     public Expr build(String s) {
-        for (int i = s.length()-1; i >= 0; i--) {
-            stack.push(Character.toString(s.charAt(i)));
+        Map<Integer, Integer> parenthesis = mapParenthesisIndex(s);
+
+        int left = 0;
+        int right = s.length()-1;
+
+        while (Character.isDigit(s.charAt(left))) {
+            left += 1;
+        }
+
+        if (s.charAt(left) == '(') {
+            
         }
 
         return new Num(0);
@@ -29,6 +40,18 @@ public class Factory {
         return new Num(0);
     }
 
+    public Expr buildFromLeft(String s, int left, int right) {
+
+
+        return new Num(0);
+    }
+
+
+    //maps index of left parenthesis to index of the corresponding right parenthesis
+    public Map mapParenthesisIndex(String s) {
+        
+        return new HashMap<>();
+    }
     
 
 }
